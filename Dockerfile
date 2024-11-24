f16e70d686bb53c7eef9bcb81e27ad6d823bab95
bluekid <bluekid0301@gmail.com>
2024-11-24 21:58:20 +0700

    u

--- Dockerfile
+++ Dockerfile
@@ -7,9 +7,12 @@
 # RUN pip install opencensus-ext-flask
 # RUN pip install opencensus-ext-logging
 # RUN pip install flask
+
 FROM tiangolo/uwsgi-nginx-flask:python3.9
+
 # Copy the content of the current directory to the /app of the container
 ADD . /app
 
 # Install depndencies 
 RUN pip install -r /app/requirements.txt
+RUN pip install --upgrade certifi
--- main.py
+++ main.py
@@ -9,36 +9,41 @@ from datetime import datetime
 
 # App Insights 
 # TODO: Import required libraries for App Insights
-from opencensus.ext.azure.log_exporter import AzureLogHandler, AzureEventHandler
 from opencensus.ext.azure import metrics_exporter
 from opencensus.ext.azure.trace_exporter import AzureExporter
 from opencensus.trace.samplers import ProbabilitySampler
 from opencensus.trace.tracer import Tracer
 from opencensus.ext.flask.flask_middleware import FlaskMiddleware
+from opencensus.ext.azure.log_exporter import AzureLogHandler
+from opencensus.ext.azure.log_exporter import AzureEventHandler
+from opencensus.stats import aggregation as aggregation_module
+from opencensus.stats import measure as measure_module
+from opencensus.stats import stats as stats_module
+from opencensus.stats import view as view_module
+from opencensus.tags import tag_map as tag_map_module
+from opencensus.trace import config_integration
 
 
-app_insights_conn_string = 'InstrumentationKey=11f82a44-a404-4049-9231-5cf22759749b;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/'
+app_insights_conn_string = 'InstrumentationKey=906ef0dd-a215-49d2-b8d9-8646738a285b'
+
+config_integration.trace_integrations(['logging'])
+config_integration.trace_integrations(['requests'])
 
 # Logging
 # TODO: Setup logger
 logger = logging.getLogger(__name__)
-logger.addHandler(AzureLogHandler(
-  connection_string="InstrumentationKey=11f82a44-a404-4049-9231-5cf22759749b;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/"
-  )
-)
-logger.addHandler(
-  AzureEventHandler(
-    connection_string="InstrumentationKey=11f82a44-a404-4049-9231-5cf22759749b;IngestionEndpoint=https://westus-0.in.applicationinsights.azure.com/"
-  )
-)
-
+logger.addHandler(AzureLogHandler(connection_string=app_insights_conn_string))
+logger.addHandler(AzureEventHandler(connection_string=app_insights_conn_string))
 logger.setLevel(logging.INFO)
 
 # Metrics
 # TODO: Setup exporter
+stats = stats_module.stats
+view_manager = stats.view_manager
 exporter = metrics_exporter.new_metrics_exporter(
     enable_standard_metrics=True,
     connection_string=app_insights_conn_string)
+view_manager.register_exporter(exporter)
 
 
 # Tracing
@@ -74,8 +79,24 @@ if ("TITLE" in os.environ and os.environ['TITLE']):
 else:
     title = app.config['TITLE']
 
-# Redis Connection
+# Comment/remove the next two lines of code.
+# Redis Connection to a local server running on the same machine where the current FLask app is running. 
 r = redis.Redis()
+# Redis configurations
+# redis_server = os.environ['REDIS']
+
+# # Redis Connection to another container
+# try:
+#   if "REDIS_PWD" in os.environ:
+#       r = redis.StrictRedis(host=redis_server,
+#                         port=6379,
+#                         password=os.environ['REDIS_PWD'])
+#   else:
+#       r = redis.Redis(redis_server)
+#   r.ping()
+# except redis.ConnectionError:
+#   exit('Failed to connect to Redis, terminating.')
+  
 
 # Change title to host name to demo NLB
 if app.config['SHOWHOST'] == "true":
@@ -93,10 +114,13 @@ def index():
         # Get current values
         vote1 = r.get(button1).decode('utf-8')
         # TODO: use tracer object to trace cat vote
-        tracer.span(name=f"Total {button1} Voted: {vote1}")
+        with tracer.span(name=f"Total {button1} Voted: {vote1}") as span:
+          print(f"Total {button1} Voted: {vote1}")
+
         vote2 = r.get(button2).decode('utf-8')
         # TODO: use tracer object to trace dog vote
-        tracer.span(name=f"Total {button2} Voted: {vote2}")
+        with tracer.span(name=f"Total {button2} Voted: {vote2}") as span:
+          print(f"Total {button2} Voted: {vote2}")
 
         # Return index with values
         return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
@@ -110,13 +134,15 @@ def index():
             r.set(button2,0)
             vote1 = r.get(button1).decode('utf-8')
             properties = {'custom_dimensions': {'Cats Vote': vote1}}
+
             # TODO: use logger object to log cat vote
-            logger.error(f"{button1} Vote")
+            logger.info(f"{button1} Vote")
 
             vote2 = r.get(button2).decode('utf-8')
             properties = {'custom_dimensions': {'Dogs Vote': vote2}}
-            logger.error(f"{button2} Vote")
+
             # TODO: use logger object to log dog vote
+            logger.info(f"{button2} Vote")
 
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
@@ -128,13 +154,16 @@ def index():
 
             # Get current values
             vote1 = r.get(button1).decode('utf-8')
+            logger.info(f"{button1} Vote")
+
             vote2 = r.get(button2).decode('utf-8')
+            logger.info(f"{button2} Vote")
 
             # Return results
             return render_template("index.html", value1=int(vote1), value2=int(vote2), button1=button1, button2=button2, title=title)
 
 if __name__ == "__main__":
     # TODO: Use the statement below when running locally
-    # app.run() 
+    app.run()
     # TODO: Use the statement below before deployment to VMSS
-    app.run(host='0.0.0.0', threaded=True, debug=True) # remote
+    # app.run(host='0.0.0.0', threaded=True, debug=True) # remote
